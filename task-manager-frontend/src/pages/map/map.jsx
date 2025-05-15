50955c4d1660b6a262d8c7023bcebf945768cd36
Grace Hanson <hansongrace2004@gmail.com>
2025-05-15 16:45:15 -0400

    add green bar

--- task-manager-frontend/src/pages/map/map.jsx
+++ task-manager-frontend/src/pages/map/map.jsx
@@ -6,18 +6,24 @@ import './map.css';
 
 function MapPage() {
   return (
-      <div className="top-wrapper">
-        
-    <div className="leaflet-map-wrapper">
-      <MapContainer center={[38.0, -98.0]} zoom={4} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    </div>
-    </div>
+    <>
+      <div className="top-bar">
+        <h1></h1>
+      </div>
 
+      <div className="leaflet-map-wrapper">
+        <MapContainer
+          center={[38.0, -98.0]}
+          zoom={4}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
+    </>
   );
 }
 
