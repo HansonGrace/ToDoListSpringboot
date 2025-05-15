4d372a57b82c0403df300868cfbc2aa9d481cbd0
Grace Hanson <hansongrace2004@gmail.com>
2025-05-15 17:51:54 -0400

    update map nav bar functions

--- task-manager-frontend/src/pages/map/map.jsx
+++ task-manager-frontend/src/pages/map/map.jsx
@@ -1,22 +1,28 @@
-// src/pages/map/map.jsx
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './map.css';
 
 function MapPage() {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="top-bar">
-        <h1></h1>
+        <div className="top-left">
+          <h1>Flight</h1>
+        </div>
+        <div className="top-right">
+          <span onClick={() => navigate('/submit')}>Submit a Spot</span>
+          <span onClick={() => navigate('/faq')}>FAQ</span>
+          <span onClick={() => navigate('/about')}>About</span>
+          <span onClick={() => navigate('/login')}>Login</span>
+        </div>
       </div>
 
       <div className="leaflet-map-wrapper">
-        <MapContainer
-          center={[38.0, -98.0]}
-          zoom={4}
-          scrollWheelZoom={true}
-        >
+        <MapContainer center={[38.0, -98.0]} zoom={4} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
